417cd6b1acd74698a734f65477108b84ed4715ce
stringguardian15 <warose91@gmail.com>
2020-02-21 03:01:10 -0800

    data_generator ready to be run. Currently running for both prefixes 'ik' and 'is'. Need to remember to include 2 copies of the stop-consonant initial roots for the recording script.

--- data_generator.py
+++ data_generator.py
@@ -1,11 +1,27 @@
+import re
 word_endings = []
 middle_vowels = []
 initial_consonants = []
 final_words = []
 root_words = []
-prefixed_words = []
+prefixes = []
 output_file = 'words.out'
 
+# a few function definitions
+def get_initial_consonant_sound(word):
+    m = re.match('([^aeiouAEIOU]*)([aeiou].*)',word)
+    initial_sound = m.group(1)
+    remaining_word = m.group(2)
+    return (initial_sound,remaining_word)
+
+def epenthesize_initial_sound(cons):
+    #if the initial cons should be epenthesized, return u+initial cons.
+    epenthesized_set = {'sh','s','f'}
+    if cons in epenthesized_set:
+        return 'u'+cons
+    else: return cons
+
+#load data
 with open('word_endings.txt','r') as fi:
     for line in fi:
         line = line.strip()
@@ -18,13 +34,11 @@ with open('initial_consonants.txt','r') as fi:
     for line in fi:
         line = line.strip()
         initial_consonants.append(line)
+with open('prefixes.txt','r') as fi:
+    for line in fi:
+        line = line.strip()
+        prefixes.append(line)
 
-def epenthesize_initial_cons(cons):
-    #if the initial cons should be epenthesized, return u+initial cons.
-    epenthesized_set = {'r','l','w'}
-    if cons in epenthesized_set:
-        return u+cons
-    else: return cons
 
 # Build the roots
 for we in word_endings:
@@ -33,6 +47,21 @@ for we in word_endings:
             w = ic+mv+we
             root_words.append(w)
 
+# build the prefix compounds
+compound_word_list = []
+for pref in prefixes:
+    for w in root_words:
+        (initial_sound, remaining_word) = get_initial_consonant_sound(w)
+        initial_sound_epenth = epenthesize_initial_sound(initial_sound)
+        root_epenth = initial_sound_epenth + remaining_word
+        compound_word = pref+root_epenth
+        compound_word_list.append(compound_word)
+final_words = root_words + compound_word_list
+
+
+
+
 with open(output_file,'w') as fo:
-    for word in root_words:
+    for word in final_words:
         fo.write(word+"\n")
+
